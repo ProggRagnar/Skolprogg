da99e95b9f740e4e585ee7723a4dab59c4f19c77
ProggRagnar <ludvig@ragnarsson.nu>
2013-03-05 22:53:56 +0100

    Update Labb #3.scm

--- Labb #3.scm
+++ Labb #3.scm
@@ -1,3 +1,5 @@
+(load "interaction-utils.scm")
+(load "table-helper.scm")
 (require (lib "trace.ss"))
 
 ;Problem 1
@@ -33,5 +35,22 @@
                        count))
         ((eq? (car lst) 'how-many-calls) count)
         ((eq? (car lst) 'reset-count) (set! count 0) count)))))
-                                
-              
+
+;Problem 4 - Se bifogat blad
+
+;Problem 5
+
+(define (make-monitored fn)
+  (let ((count 0))
+    (lambda lst
+      (cond
+        ((null? lst) (fn))
+        ((eq? (car lst) 'how-many-calls) count)
+        ((eq? (car lst) 'reset) (set! count 0))
+        (else (begin
+                (set! count (+ count 1))
+                (apply fn lst)))))))
+
+;Problem 6 - Se bifogat blad
+
+;Problem 7
